fda0841d0bcc3cf6a61bc477ed071825f7e318a4
Camden Blatchly <camdenblatchly@gmail.com>
2025-07-10 14:40:47 -0400

    attempt at adding a label, failed

--- _site/pages/citycenters/config.js
+++ _site/pages/citycenters/config.js
@@ -16,16 +16,21 @@ var config = {
     use3dTerrain: false, //set true for enabling 3D maps.
     auto: false,
     title: "Where is Boston's city center?",
-    subtitle: 'A simple analysis of a surprisingly complex question',
+    subtitle: 'Getting to the bottom of a surprisingly complex question',
     byline: 'By Camden Blatchly',
     footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
     chapters: [
         {
             id: 'slug-style-id',
-            alignment: 'left',
+            alignment: 'fully',
             hidden: false,
-            title: 'San Francisco',
-            description: 'The first chapter contains a title, image, and camera view for San Francisco, California. Update the chapter data to make it your own.',
+            description: `A few weeks ago, while explaining to a friend 
+            where I live relative to ‘downtown’ Boston, I got philosophical. 
+            I wondered, <em>what even is downtown</em>? We refer to a city’s center 
+            all the time, but abstractly. It’s a know-it-when-you-see-it 
+            type phenomenon. Sometimes though, you need to be specific, like 
+            when planning infrastructure or conducting economic research. 
+            How then should we define the city center? I tried to find out.`,
             location: {
                 center: [-71.08131, 42.33994],
                 zoom: 8.5,
@@ -34,14 +39,14 @@ var config = {
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
-            callback: '',
+            callback: 'hideAllDefinitions',
             showMarker: true,
             onChapterEnter: [
-                // {
-                //     layer: 'layer-name',
-                //     opacity: 1,
-                //     duration: 5000
-                // }
+                {
+                    layer: 'city-centers',
+                    'circle-opacity': 0,
+                    'circle-stroke-opacity': 0
+                }
             ],
             onChapterExit: [
                 // {
@@ -52,36 +57,33 @@ var config = {
         },
         {
             id: 'second-identifier',
-            alignment: 'right',
+            alignment: 'left',
             hidden: false,
-            title: 'Washington, D.C.',
-            image: './assets/washington-dc.jpg',
-            description: 'The second chapter flies to Washington, D.C., updates the camera pitch, and slowly rotates. <br>  <br> Washington, D.C., the capital of the United States, is a vibrant city known for its iconic landmarks, including the White House, the U.S. Capitol, and the Washington Monument. It serves as the political heart of the nation and a center for history, culture, and international diplomacy.',
+            description: 'What does this mean?',
             location: {
                 center: [-71.08131, 42.33994],
-                zoom: 8.5,
-                pitch: 60,
-                bearing: -43.2,
-                // flyTo additional controls-
-                // These options control the flight curve, making it move
-                // slowly and zoom out almost completely before starting
-                // to pan.
-                //speed: 2, // make the flying slow
-                //curve: 1, // change the speed at which it zooms out
+                zoom: 12.52,
+                pitch: 8.01,
+                bearing: 0.00
             },
             mapAnimation: 'flyTo',
             rotateAnimation: true,
-            callback: 'filterGoogleEarth',
-            onChapterEnter: [],
+            callback: 'showGoogleEarthDefinition',
+            onChapterEnter: [
+                {
+                    layer: 'city-centers',
+                    'circle-opacity': .9,
+                    'circle-stroke-opacity': 1
+                }
+            ],
             onChapterExit: []
         },
         {
             id: 'third-identifier',
             alignment: 'left',
             hidden: false,
-            title: 'Geneva',
-            image: './assets/geneva.jpg',
-            description: 'Geneva, Switzerland, is a picturesque city nestled along the shores of Lake Geneva, surrounded by the Alps and Jura mountains. Known as a global hub for diplomacy and finance, it is home to numerous international organizations, including the United Nations and the Red Cross.',
+            title: 'Population',
+            description: 'Something about how I calculated this',
             location: {
                 center: [-71.08131, 42.33994],
                 zoom: 12.52,
@@ -90,26 +92,61 @@ var config = {
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
-            callback: '',
+            callback: 'showPopulationDefinition',
             onChapterEnter: [],
             onChapterExit: []
         },
         {
             id: 'fourth-chapter',
-            alignment: 'fully',
+            alignment: 'left',
             hidden: false,
-            title: 'Buenos Aires',
-            image: './assets/buenos-aires.jpg',
+            title: 'Tall Buildings',
             description: 'Buenos Aires, the capital of Argentina, is a dynamic city known for its European-inspired architecture, vibrant tango culture, and rich culinary scene. Often called the "Paris of South America," it blends historic charm with modern energy.  You can add as many chapters as you need, just copy the JSON data and make changes.',
             location: {
                 center: [-71.08131, 42.33994],
-                zoom: 4,
-                pitch: 0,
-                bearing: 0
+                zoom: 12.52,
+                pitch: 8.01,
+                bearing: 0.00
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
-            callback: '',
+            callback: 'showBuildingHeightDefinition',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'fifth-chapter',
+            alignment: 'left',
+            hidden: false,
+            title: 'Central Business District',
+            description: 'Buenos Aires, the capital of Argentina, is a dynamic city known for its European-inspired architecture, vibrant tango culture, and rich culinary scene. Often called the "Paris of South America," it blends historic charm with modern energy.  You can add as many chapters as you need, just copy the JSON data and make changes.',
+            location: {
+                center: [-71.08131, 42.33994],
+                zoom: 12.52,
+                pitch: 8.01,
+                bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: 'showCBDDefinition',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+                {
+            id: 'sixth-chapter',
+            alignment: 'left',
+            hidden: false,
+            title: 'Gazetteer',
+            description: 'Buenos Aires, the capital of Argentina, is a dynamic city known for its European-inspired architecture, vibrant tango culture, and rich culinary scene. Often called the "Paris of South America," it blends historic charm with modern energy.  You can add as many chapters as you need, just copy the JSON data and make changes.',
+            location: {
+                center: [-71.08131, 42.33994],
+                zoom: 12.52,
+                pitch: 8.01,
+                bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: 'showGazetteerDefinition',
             onChapterEnter: [],
             onChapterExit: []
         }
